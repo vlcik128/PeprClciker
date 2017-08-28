852eb89f51be412a6aec47a473349ff739068697
vlcik128 <vlckoadam.o.k.m@outlook.sk>
2017-08-28 22:11:01 +0200

    Create Main.java

---
+++ vlcik128/pepr/clicker/Main.java
@@ -0,0 +1,14 @@
+package vlcik128.pepr.clicker;
+
+public class Main {
+	private static void log(String s){
+		System.out.println(s);
+	}
+	public static void main(String[] args){
+		log("PepřClicker BETA 0.1 by vlcik128 is going to be enabled.");
+		log("System aktualizacii sa pripravuje");
+		// Aktualizacie
+		@SuppressWarnings("unused")
+		PeprClicker pc = new PeprClicker();
+	}
+}
